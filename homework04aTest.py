e777afae4f1e405eba21c33a978ea425388f3815
unknown <voduy12@gmail.com>
2020-02-25 21:33:38 -0500

    ignore this, just switching computers

--- homework04aTest.py
+++ homework04aTest.py
@@ -1,11 +1,17 @@
 import unittest
 from homework04a import getCommits
 import math
+from unittest.mock import Mock
 
 class TestHomework(unittest.TestCase):
-    def test_get_repos_commit(self):    
-        self.assertEqual(getCommits('vodewey12','Classify-Triangle'), 'Repo: Classify-Triangle Number of commits: 4')
+    @mock.patch('homework04a.getCommits')
+    def test_get_repos_commit(self):
+        gitHub = [{
+            'user'
+        }]
+        
+
+        self.assertEqual(getCommits('vodewey12','Classify-Triangle'), 'Repo: Classify-Triangle Number of commits: 6')
         self.assertEqual(getCommits('vodewey12','Leet-Code-Practice'), 'Repo: Leet-Code-Practice Number of commits: 6')
-        self.assertEqual(getCommits('vodewey12','Meal-Exchange'), 'Repo: Meal-Exchange Number of commits: 17')
 if __name__ == '__main__':
     unittest.main()
